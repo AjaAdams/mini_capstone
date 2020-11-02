97bb5c7d50c990bd8fd82f7a171d115e1d195626
Aja <ajajhendrix@gmail.com>
2020-11-02 13:49:51 -0600

    adds restful actions for create, update, and delete

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -10,6 +10,41 @@ class Api::ProductsController < ApplicationController
     render "show.json.jb"
   end
 
+  def create
+    @product = Product.create ({
+      artist: params["artist"],
+      title: params["title"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+      year: params["year"],
+    })
+    render "show.json.jb"
+  end
+
+  def update
+    input_value = params["id"]
+    @product = Product.find_by(id: input_value)
+    @product.artist = params["artist"] || @product.artist
+    @product.title = params["title"] || @product.title
+    @product.price = params["price"] || @product.price
+    @product.genre = params["genre"] || @product.genre
+    @product.description = params["description"] || @product.description
+    @product.image_url = params["image_url"] || @product.image_url
+
+    @product.save
+
+    render "show.json.jb"
+  end
+
+  def destroy
+    input_value = params["id"]
+    @product = Product.find_by(id: input_value)
+    @product.destroy
+
+    render json: { message: "You have successfully deleted the product!" }
+  end
+
   # def single_product_url
   #   input_value = params[:id]
   #   @product = Product.find_by(id: input_value)
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
     get "/products" => "products#index"
     get "/product_path" => "products#single_product_query"
     get "/products/:id" => "products#show"
+    post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
   end
 end
