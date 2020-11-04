725143cf14f85160b42da26f355113867761d10b
Aja <ajajhendrix@gmail.com>
2020-11-04 12:18:14 -0600

    adds validations to model file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_184515) do
+ActiveRecord::Schema.define(version: 2020_11_03_212535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,13 +18,14 @@ ActiveRecord::Schema.define(version: 2020_10_28_184515) do
   create_table "products", force: :cascade do |t|
     t.string "artist"
     t.string "title"
-    t.integer "price"
+    t.decimal "price", precision: 5, scale: 2
     t.string "genre"
     t.integer "year"
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
 end
